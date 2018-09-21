f5b137db9c9fb5989839516895570956862d8fb8
PC <PC@PC-PC>
2018-09-21 21:28:04 +0200

    21.9

--- src/konstante/Operacije.java
+++ src/konstante/Operacije.java
@@ -10,44 +10,45 @@ package konstante;
  * @author PC
  */
 public class Operacije {
-    public  static final int DA_LI_JE_REGISTROVAN_SLUZBENIK=1;
-    public  static final int UNOS_KOMISIJE=2;
-    public  static final int IZMENA_KOMISIJE=3;
-    public  static final int UNOS_KANDIDATA=4;
-    public  static final int UNOS_GRUPE_ZADATAKA=5;
-    public  static final int VRATI_CLANOVE=6;
-    public  static final int VRATI_MAX_ID_KOMISIJA=7;
-    public  static final int VRATI_KOMISIJU=8;
-    public  static final int VRATI_NAC=9;
-    public  static final int VRATI_DRZ=10;
-    public  static final int VRATI_SIF_ZANIM=11;
-    public  static final int VRATI_SIF_SS=12;
-     public  static final int VRATI_TEST=13;
-     public  static final int VRATI_TESTA_LI_JE_REGISTROVANA_KOMISIJA=14;
-     public  static final int DA_LI_POSTOJI_KOMISIJA=15;
-     public  static final int VRATI_MAX_ID_KARTONA=16;
-     public  static final int VRATI_GRUPU_ZADATKA=17;
-     public  static final int SACUVAJ_KARTON=18;
-     public  static final int VRATI_KARTON = 19;
-      public  static final int VRATI_KARTON_UNOS_1 = 20;
-      public  static final int VRATI_KANDIDATA = 21;
-      public  static final int SACUVAJ_KARTON_VERIFIKACIJA = 22;
-      
-      public  static final int IZMENI_ZADATKE = 23;
-      public  static final int SPOJ_KARTONE = 24;
-      
-      public  static final int VRATI_KARTONE_PROVERA = 25;
-      public  static final int IZRACUNAJ_POENE = 26;
-      
-      public  static final int NAPRAVI_RANG_LISTU = 27;
-      public  static final int NADJI_PRIJAVLJENOG = 28;
-      
-      public  static final int KREIRAJ_RL_ISIT = 29;
-      public  static final int KREIRAJ_RL_MEN = 30;
-      public  static final int KREIRAJ_RL_ISIT_DALJ = 31;
-      public  static final int SACUVAJ_OBAVESTENJA = 32;
-      
-      
-      public  static final int VRATI_RANG_LISTU = 33;
-     
+
+    public static final int DA_LI_JE_REGISTROVAN_SLUZBENIK = 1;
+    public static final int UNOS_KOMISIJE = 2;
+    public static final int IZMENA_KOMISIJE = 3;
+    public static final int UNOS_KANDIDATA = 4;
+    public static final int UNOS_GRUPE_ZADATAKA = 5;
+    public static final int VRATI_CLANOVE = 6;
+    public static final int VRATI_MAX_ID_KOMISIJA = 7;
+    public static final int VRATI_KOMISIJU = 8;
+    public static final int VRATI_NAC = 9;
+    public static final int VRATI_DRZ = 10;
+    public static final int VRATI_SIF_ZANIM = 11;
+    public static final int VRATI_SIF_SS = 12;
+    public static final int VRATI_TEST = 13;
+    public static final int VRATI_TESTA_LI_JE_REGISTROVANA_KOMISIJA = 14;
+    public static final int DA_LI_POSTOJI_KOMISIJA = 15;
+    public static final int VRATI_MAX_ID_KARTONA = 16;
+    public static final int VRATI_GRUPU_ZADATKA = 17;
+    public static final int SACUVAJ_KARTON = 18;
+    public static final int VRATI_KARTON = 19;
+    public static final int VRATI_KARTON_UNOS_1 = 20;
+    public static final int VRATI_KANDIDATA = 21;
+    public static final int SACUVAJ_KARTON_VERIFIKACIJA = 22;
+
+    public static final int IZMENI_ZADATKE = 23;
+    public static final int SPOJ_KARTONE = 24;
+
+    public static final int VRATI_KARTONE_PROVERA = 25;
+    public static final int IZRACUNAJ_POENE = 26;
+
+    public static final int NAPRAVI_RANG_LISTU = 27;
+    public static final int NADJI_PRIJAVLJENOG = 28;
+
+    public static final int KREIRAJ_RL_ISIT = 29;
+    public static final int KREIRAJ_RL_MEN = 30;
+    public static final int KREIRAJ_RL_ISIT_DALJ = 31;
+    public static final int SACUVAJ_OBAVESTENJA = 32;
+
+    public static final int VRATI_RANG_LISTU = 33;
+    public static final int VRATI_KANDIDATA_ZA_VERIFIKACIJU = 34;
+
 }
